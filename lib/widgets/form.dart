0d89f78e0eb8f140181b5912519a2c440345f14c
Shubham Yadav <100077254+Shubham185y@users.noreply.github.com>
2023-07-20 22:38:30 +0530

    working with backend added

--- lib/widgets/form.dart
+++ lib/widgets/form.dart
@@ -1,7 +1,9 @@
-// ignore_for_file: prefer_const_constructors, unnecessary_nullable_for_final_variable_declarations, use_key_in_widget_constructors, library_private_types_in_public_api, unused_import
+// ignore_for_file: prefer_const_constructors, unnecessary_nullable_for_final_variable_declarations, use_key_in_widget_constructors, library_private_types_in_public_api, unused_import, use_build_context_synchronously, prefer_const_declarations, avoid_print, unused_field, prefer_final_fields
 import 'dart:io';
 import 'package:flutter/material.dart';
 import 'package:image_picker/image_picker.dart';
+import 'package:http/http.dart' as http;
+import 'package:flutter_spinkit/flutter_spinkit.dart';
 
 void main() {
   runApp(MaterialApp(
@@ -18,7 +20,7 @@ class _MyFormPageState extends State<MyFormPage> {
   String? selectedYear;
   String? selectedSemester;
   String? selectedSubject;
-
+   bool _isLoading = false;
   List<String> years = ['First Year', 'Second Year', 'Third Year'];
 
   Map<String, List<String>> semesterOptions = {
@@ -61,168 +63,266 @@ class _MyFormPageState extends State<MyFormPage> {
     return selectedYear != null && selectedSemester != null && selectedSubject != null;
   }
 
-  Future<void> _submitPhotos() async {
-    if (_imageList == null || _imageList!.isEmpty) {
-      showDialog(
-        context: context,
-        builder: (BuildContext context) {
-          return AlertDialog(
-            title: Text('Error'),
-            content: Text('Please select at least one photo.'),
-            actions: [
-              TextButton(
-                child: Text('OK'),
-                onPressed: () {
-                  Navigator.of(context).pop();
-                },
-              ),
-            ],
-          );
-        },
-      );
-      return;
+ Future<void> _submitPhotos() async {
+  if (_imageList == null || _imageList!.isEmpty) {
+    showDialog(
+      context: context,
+      builder: (BuildContext context) {
+        return AlertDialog(
+          title: Text('Error'),
+          content: Text('Please select at least one photo.'),
+          actions: [
+            TextButton(
+              child: Text('OK'),
+              onPressed: () {
+                Navigator.of(context).pop();
+              },
+            ),
+          ],
+        );
+      },
+    );
+    return;
+  }
+
+  // Perform face detection and recognition with the selected photos and send to server
+  try {
+    // Show loading screen
+    setState(() {
+      _isLoading = true;
+    });
+
+    for (var image in _imageList!) {
+      // Read the image file
+      final File file = File(image.path);
+      List<int> imageBytes = await file.readAsBytes();
+
+      // Build the multipart request
+      final String url = 'http://192.168.1.207:8000/api/upload_image'; // Replace with your server's endpoint
+      var request = http.MultipartRequest('POST', Uri.parse(url));
+
+      // Attach the image file as a multipart file
+      request.files.add(http.MultipartFile.fromBytes('image', imageBytes, filename: image.path));
+
+      // Add additional attributes to the request
+      request.fields['year'] = selectedYear!;
+      request.fields['semester'] = selectedSemester!;
+      request.fields['subject'] = selectedSubject!;
+
+      // Send the request
+      var response = await request.send();
+
+      // Check the response status
+      if (response.statusCode == 200) {
+        // Image uploaded successfully
+        print('Image uploaded successfully');
+      } else {
+        // Handle the error case
+        print('Image upload failed with status code ${response.statusCode}');
+      }
     }
 
-    // Perform face detection and recognition with the selected photos and send to server
-    // Write your code here to process the images and communicate with the server
-
-    // Clear the selected images after submission
+    // Clear the selected images after successful submission
     setState(() {
       _imageList = null;
     });
+
+    // Hide loading screen
+    setState(() {
+      _isLoading = false;
+    });
+
+    // Show a pop-up message for successful submission
+    showDialog(
+      context: context,
+      builder: (BuildContext context) {
+        return AlertDialog(
+          title: Text('Success'),
+          content: Text('Image submitted successfully.'),
+          actions: [
+            TextButton(
+              child: Text('OK'),
+              onPressed: () {
+                Navigator.of(context).pop();
+              },
+            ),
+          ],
+        );
+      },
+    );
+  } catch (e) {
+    print('Error uploading image: $e');
+    // Hide loading screen
+    setState(() {
+      _isLoading = false;
+    });
+
+    showDialog(
+      context: context,
+      builder: (BuildContext context) {
+        return AlertDialog(
+          title: Text('Error'),
+          content: Text('Failed to submit image.'),
+          actions: [
+            TextButton(
+              child: Text('OK'),
+              onPressed: () {
+                Navigator.of(context).pop();
+              },
+            ),
+          ],
+        );
+      },
+    );
   }
+}
 
   @override
-  Widget build(BuildContext context) {
-    return Scaffold(
-      body: SingleChildScrollView(
-        child: Padding(
-          padding: EdgeInsets.all(16.0),
-          child: Column(
-            children: [
-              Text(
-                'Choose a Year:',
-                style: TextStyle(fontSize: 18.0),
-              ),
-              DropdownButtonFormField<String>(
-                value: selectedYear,
-                onChanged: (String? newValue) {
-                  setState(() {
-                    selectedYear = newValue;
-                    selectedSemester = null;
-                    selectedSubject = null;
-                  });
-                },
-                items: years.map((String year) {
-                  return DropdownMenuItem<String>(
-                    value: year,
-                    child: Text(year),
-                  );
-                }).toList(),
-              ),
-              SizedBox(height: 20.0),
-              if (selectedYear != null)
-                Column(
-                  children: [
-                    Text(
-                      'Choose a Semester:',
-                      style: TextStyle(fontSize: 18.0),
-                    ),
-                    DropdownButtonFormField<String>(
-                      value: selectedSemester,
-                      onChanged: (String? newValue) {
-                        setState(() {
-                          selectedSemester = newValue;
-                          selectedSubject = null;
-                        });
-                      },
-                      items: semesterOptions[selectedYear]!.map((String semester) {
-                        return DropdownMenuItem<String>(
-                          value: semester,
-                          child: Text(semester),
-                        );
-                      }).toList(),
-                    ),
-                  ],
+Widget build(BuildContext context) {
+  return Scaffold(
+    body: Stack(
+      children: [
+        SingleChildScrollView(
+          child: Padding(
+            padding: EdgeInsets.all(16.0),
+            child: Column(
+              children: [
+                Text(
+                  'Choose a Year:',
+                  style: TextStyle(fontSize: 18.0),
                 ),
-              SizedBox(height: 20.0),
-              if (selectedSemester != null)
-                Column(
-                  children: [
-                    Text(
-                      'Choose a Subject:',
-                      style: TextStyle(fontSize: 18.0),
-                    ),
-                    DropdownButtonFormField<String>(
-                      value: selectedSubject,
-                      onChanged: (String? newValue) {
-                        setState(() {
-                          selectedSubject = newValue;
-                        });
-                      },
-                      items: subjectOptions[selectedSemester]!.map((String subject) {
-                        return DropdownMenuItem<String>(
-                          value: subject,
-                          child: Text(subject),
-                        );
-                      }).toList(),
-                    ),
-                  ],
+                DropdownButtonFormField<String>(
+                  value: selectedYear,
+                  onChanged: (String? newValue) {
+                    setState(() {
+                      selectedYear = newValue;
+                      selectedSemester = null;
+                      selectedSubject = null;
+                    });
+                  },
+                  items: years.map((String year) {
+                    return DropdownMenuItem<String>(
+                      value: year,
+                      child: Text(year),
+                    );
+                  }).toList(),
                 ),
-              SizedBox(height: 20.0),
-              if (isAllDropdownSelected())
-                Column(
-                  children: [
-                    ElevatedButton.icon(
-                      onPressed: _openCamera,
-                      icon: Icon(Icons.camera),
-                      label: Text('Take Photo'),
-                      style: ElevatedButton.styleFrom(
-                        minimumSize: Size(150, 50),
-                        shape: RoundedRectangleBorder(
-                          borderRadius: BorderRadius.circular(8.0),
-                        ),
+                SizedBox(height: 20.0),
+                if (selectedYear != null)
+                  Column(
+                    children: [
+                      Text(
+                        'Choose a Semester:',
+                        style: TextStyle(fontSize: 18.0),
                       ),
-                    ),
-                    SizedBox(height: 20.0),
-                    ElevatedButton.icon(
-                      onPressed: _openGallery,
-                      icon: Icon(Icons.photo_library),
-                      label: Text('Select Photos'),
-                      style: ElevatedButton.styleFrom(
-                        minimumSize: Size(150, 50),
-                        shape: RoundedRectangleBorder(
-                          borderRadius: BorderRadius.circular(8.0),
-                        ),
-                      ),
-                    ),
-                    SizedBox(height: 20.0),
-                    if (_imageList != null)
-                      Column(
-                        children: _imageList!.map((XFile image) {
-                          return Container(
-                            width: 200.0,
-                            height: 200.0,
-                            decoration: BoxDecoration(
-                              image: DecorationImage(
-                                image: FileImage(File(image.path)),
-                                fit: BoxFit.cover,
-                              ),
-                            ),
+                      DropdownButtonFormField<String>(
+                        value: selectedSemester,
+                        onChanged: (String? newValue) {
+                          setState(() {
+                            selectedSemester = newValue;
+                            selectedSubject = null;
+                          });
+                        },
+                        items: semesterOptions[selectedYear]!.map((String semester) {
+                          return DropdownMenuItem<String>(
+                            value: semester,
+                            child: Text(semester),
                           );
                         }).toList(),
                       ),
-                    SizedBox(height: 20.0),
-                    ElevatedButton(
-                      onPressed: _submitPhotos,
-                      child: Text('Submit'),
-                    ),
-                  ],
-                ),
-            ],
+                    ],
+                  ),
+                SizedBox(height: 20.0),
+                if (selectedSemester != null)
+                  Column(
+                    children: [
+                      Text(
+                        'Choose a Subject:',
+                        style: TextStyle(fontSize: 18.0),
+                      ),
+                      DropdownButtonFormField<String>(
+                        value: selectedSubject,
+                        onChanged: (String? newValue) {
+                          setState(() {
+                            selectedSubject = newValue;
+                          });
+                        },
+                        items: subjectOptions[selectedSemester]!.map((String subject) {
+                          return DropdownMenuItem<String>(
+                            value: subject,
+                            child: Text(subject),
+                          );
+                        }).toList(),
+                      ),
+                    ],
+                  ),
+                SizedBox(height: 20.0),
+                if (isAllDropdownSelected())
+                  Column(
+                    children: [
+                      ElevatedButton.icon(
+                        onPressed: _openCamera,
+                        icon: Icon(Icons.camera),
+                        label: Text('Take Photo'),
+                        style: ElevatedButton.styleFrom(
+                          minimumSize: Size(150, 50),
+                          shape: RoundedRectangleBorder(
+                            borderRadius: BorderRadius.circular(8.0),
+                          ),
+                        ),
+                      ),
+                      SizedBox(height: 20.0),
+                      ElevatedButton.icon(
+                        onPressed: _openGallery,
+                        icon: Icon(Icons.photo_library),
+                        label: Text('Select Photos'),
+                        style: ElevatedButton.styleFrom(
+                          minimumSize: Size(150, 50),
+                          shape: RoundedRectangleBorder(
+                            borderRadius: BorderRadius.circular(8.0),
+                          ),
+                        ),
+                      ),
+                      SizedBox(height: 20.0),
+                      if (_imageList != null)
+                        Column(
+                          children: _imageList!.map((XFile image) {
+                            return Container(
+                              width: 200.0,
+                              height: 200.0,
+                              decoration: BoxDecoration(
+                                image: DecorationImage(
+                                  image: FileImage(File(image.path)),
+                                  fit: BoxFit.cover,
+                                ),
+                              ),
+                            );
+                          }).toList(),
+                        ),
+                      SizedBox(height: 20.0),
+                      ElevatedButton(
+                        onPressed: _submitPhotos,
+                        child: Text('Submit'),
+                      ),
+                    ],
+                  ),
+              ],
+            ),
           ),
         ),
-      ),
-    );
-  }
+        if (_isLoading)
+          Container(
+            color: Colors.black.withOpacity(0.5),
+            child: Center(
+              child: SpinKitFadingCube(
+                color: Colors.blue[300],
+                size: 50.0,
+              ),
+            ),
+          ),
+      ],
+    ),
+  );
+}
+
 }
